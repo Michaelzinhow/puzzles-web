75649dc576106596dfc06fd841dd723347892f31
Michael <mnpa1984@gmail.com>
2017-10-02 23:11:25 -0400

    importação de mixins

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+//task que chama o sass
+gulp.task('sass', function(){
+	return gulp.src('sass/**/*.sass')
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(gulp.dest('css'));
+	
+});
+
+//task que chama o watch
+gulp.task('watch', function(){
+	gulp.watch('sass/**/*.sass', ['sass']);
+});
+
+//task default que chama o gulp
+gulp.task('default', ['sass', 'watch']); 
